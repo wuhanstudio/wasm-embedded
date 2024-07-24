24923ce68af671d987e783b05543d3a4e849ce7d
Wu Han <wuhanstudio@qq.com>
2024-07-25 00:36:08 +0200

    [Arduino] Add dino example

--- arduino/esp32_tft_dino/esp32_tft_dino.ino
+++ arduino/esp32_tft_dino/esp32_tft_dino.ino
@@ -4,13 +4,12 @@
    All rights reserved.
 */
 
+
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
 #include <SPI.h>
 
 #include <wasm3.h>
-
-#include "fire.wasm.h"
 
 #define NATIVE_STACK_SIZE   (32*1024)
 
@@ -19,14 +18,41 @@
 
 #define DISPLAY_BRIGHTESS   128    // 0..255
 
+
+#if defined(ARDUINO_FEATHER_ESP32) // Feather Huzzah32
+#define TFT_CS         14
+#define TFT_RST        15
+#define TFT_DC         32
+
+#elif defined(ESP8266)
+#define TFT_CS         4
+#define TFT_RST        16
+#define TFT_DC         5
+
+#else
+// For the breakout board, you can use any 2 or 3 pins.
+// These pins will also work for the 1.8" TFT shield.
 #define TFT_CS         26
 #define TFT_RST        16 // Or set to -1 and connect to Arduino RESET pin
 #define TFT_DC         17
+#endif
 
 #define TFT_LED  13
 
 // For 1.44" and 1.8" TFT with ST7735 use:
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+/*
+   Dino game by by Ben Smith (binji)
+     https://github.com/binji/raw-wasm/tree/master/dino
+   To build:
+     export PATH=/opt/wasp/build/src/tools:$PATH
+     wasp wat2wasm --enable-numeric-values -o dino.wasm dino.wat
+     xxd -iC dino.wasm > dino.wasm.h
+
+   Note: In Arduino IDE, select Tools->Optimize->Faster (-O3)
+*/
+#include "dino.wasm.h"
 
 /*
    Engine start, liftoff!
@@ -49,7 +75,7 @@ m3ApiRawFunction(Math_random)
 }
 
 // Memcpy is generic, and much faster in native code
-m3ApiRawFunction(Fire_memcpy)
+m3ApiRawFunction(Dino_memcpy)
 {
   m3ApiGetArgMem  (uint8_t *, dst)
   m3ApiGetArgMem  (uint8_t *, src)
@@ -79,17 +105,17 @@ void load_wasm()
 
   m3_FreeRuntime(runtime);
 
-  runtime = m3_NewRuntime (env, 80 * 1024, NULL);
+  runtime = m3_NewRuntime (env, 1024, NULL);
   if (!runtime) FATAL("NewRuntime", "failed");
 
-  result = m3_ParseModule (env, &module, fire_16_bit_wasm, sizeof(fire_16_bit_wasm));
+  result = m3_ParseModule (env, &module, dino_wasm, sizeof(dino_wasm));
   if (result) FATAL("ParseModule", result);
 
   result = m3_LoadModule (runtime, module);
   if (result) FATAL("LoadModule", result);
 
   m3_LinkRawFunction (module, "Math",   "random",     "f()",      &Math_random);
-  m3_LinkRawFunction (module, "Fire",   "memcpy",     "v(iii)",   &Fire_memcpy);
+  m3_LinkRawFunction (module, "Dino",   "memcpy",     "v(iii)",   &Dino_memcpy);
 
   mem = m3_GetMemory (runtime, NULL, 0);
   if (!mem) FATAL("GetMemory", "failed");
@@ -107,14 +133,13 @@ void init_device()
   randomSeed((analogRead(A5) << 16) + analogRead(A4));
   Serial.print("Random: 0x"); Serial.println(random(INT_MAX), HEX);
 
+
   pinMode(TFT_LED, OUTPUT);
   digitalWrite(TFT_LED, HIGH);
 
   tft.initR(INITR_144GREENTAB); // Init ST7735R chip, green tab
   tft.setRotation(1);
-  // tft.setSwapBytes(true);
 }
-
 
 void setup()
 {
@@ -127,7 +152,8 @@ void setup()
   init_device();
   TFINISH("Device init");
 
-  tft.fillScreen(ST77XX_BLACK);
+  tft.fillScreen(ST77XX_WHITE);
+  tft.setTextColor(ST77XX_BLACK, ST77XX_WHITE);
 
   TSTART();
   load_wasm();
@@ -160,13 +186,8 @@ void wasm_task(void*)
     result = m3_CallV (func_run);
     if (result) break;
 
-    // Output to display (Big Endian)
-    tft.startWrite();
-    tft.writePixels((uint16_t*)(mem + 0x4000), 128 * 128, true, true);
-    tft.endWrite();
-
-    // Output to display (Little Endian)
-    // tft.drawRGBBitmap(0, 0, (uint16_t*)(mem + 0x4000), 128, 128);
+    // Output to display
+    tft.drawRGBBitmap(0, 135 - 10 - 75, (uint16_t*)(mem + 0x5000), 240, 75);
 
     const uint64_t frametime = micros() - framestart;
     const uint32_t target_frametime = 1000000 / 50;
